f2405c0bd565bdd532c7dc5685c7895af89db365
Dharmanshu <manshuk89@gmail.com>
2015-06-10 22:59:03 -0400

    unicode issue resolved

--- test.py
+++ test.py
@@ -1,81 +1,89 @@
 from framework import *
 import csv,codecs, unicodedata
 
-i = 0
-# out_file = open("test.json","w"):
-
-min_id = None
-
-with (codecs.open('test.csv' , 'w',encoding ='utf-8',errors='ignore')) as fp:
-	f = csv.writer(fp , delimiter=',')
-	f.writerow (["tweet ID","user ID","tweet coordinates",
-				"friend_Count","location","geo_enabled",
-				"Screen name","user name"])
-	t = TwitterFW()
-	url ="https://api.twitter.com/1.1/search/tweets.json"
-	geocode = "40.717728,-74.0021647,100mi"
+def fetch_tweets_till_date() :
 	
-	while (i< 10):
-		if (min_id == None):
-			resp, content = t.get(
-									url , 
-									{'geocode' : geocode ,
-									}
-								)
-		else :
-			resp, content = t.get(
-									url , 
-									{'geocode' : geocode , 
-									'max_id' : str(min_id)
-									}
-								)
+	i = 0
 
-		# printing the contents
+	min_id = None
+	with (codecs.open('test.csv' , 'w',encoding ='utf-8',errors='ignore')) as fp:
+		f = csv.writer(fp , delimiter=',')
+		f.writerow (["tweet ID","user ID","tweet coordinates",
+					"friend_Count","location","geo_enabled",
+					"Screen name","user name"])
+		t = TwitterFW()
+		url ="https://api.twitter.com/1.1/search/tweets.json"
 	
-		# pprint(json.loads(content))
-		content = unicode(content,"utf-8")
-		content = json.loads(content)
-		# for tweet in content['statuses'] :
-		# 	print "Tweet id is " + str(tweet['id']) + " by " + str(tweet['user']['screen_name'])
-		
-		for x in range(0 , len(content['statuses'])):
-			lat_lng = content['statuses'][x].get('coordinates',None)
-			if lat_lng :
-				lat_lng = lat_lng.get('coordinates')
-				lat_lng_str = str(lat_lng[0]) + '-' + str(lat_lng[1])
+		geocode = "40.717728,-74.0021647,100mi"
+		until = "2014-09-01"
+	
+		while (i < 10):
+			if (min_id == None):
+				resp, content = t.get(
+										url , 
+										{
+											'geocode' : geocode ,
+											'until' : until
+										}
+									)
 			else :
-				lat_lng_str = ''
+				resp, content = t.get(
+										url , 
+										{'geocode' : geocode , 
+										'max_id' : str(min_id)
+										}
+									)
+			# printing the contents
+			# pprint(json.loads(content))
+			content = unicode(content,"utf-8")
+			content = json.loads(content)
+			if not content['statuses'] :
+				print("End of tweets encountered.")
+				return
 			
-			s = [
-			content['statuses'][x]['id'],
-			content['statuses'][x]['user']['id'] ,
-			lat_lng_str,
-			content['statuses'][x]['user']['friends_count'],
-			content['statuses'][x]['user']['location'],
-			content['statuses'][x]['user']['geo_enabled'],
-			content['statuses'][x]['user']['screen_name'],
-			content['statuses'][x]['user']['name'],
-			]
-			# print s
-			# for i in range(0 , len(s)):
-				# s[i] = unicode(s[i]).encode("utf-8")
-			
-			# print s
-			f.writerow([unicode(s).encode("utf-8")])
-			# f.writerow(s)
-			# unicode(content['statuses'][x]['user']['screen_name']).replace("\/","/"),
-			# unicode(content['statuses'][x]['user']['name'] , errors='ignore'),
-			# str(content['statuses'][x]['geo']['coordinates'][0]) + ',' + str(content['statuses'][x]['geo']['coordinates'][1]),
-			# content['statuses'][x]['place']['full_name'],
-			# content['statuses'][x]['place']['country'].encode("utf-8")
+			# for tweet in content['statuses'] :
+			# 	print "Tweet id is " + str(tweet['id']) + " by " + str(tweet['user']['screen_name'])
 		
-		last_index = len(content['statuses']) -1
-		min_id = content['statuses'][last_index]['id']
-		max_id = content['statuses'][0]['id']
-		min_id = min_id -1
+			for x in range(0 , len(content['statuses'])):
+				lat_lng = content['statuses'][x].get('coordinates',None)
+				if lat_lng :
+					lat_lng = lat_lng.get('coordinates')
+					lat_lng_str = str(lat_lng[0]) + '-' + str(lat_lng[1])
+				else :
+					lat_lng_str = ''
+			
+				s = [
+				content['statuses'][x]['id'],
+				content['statuses'][x]['user']['id'] ,
+				lat_lng_str,
+				content['statuses'][x]['user']['friends_count'],
+				content['statuses'][x]['user']['location'].encode("ascii" , "ignore"),
+				content['statuses'][x]['user']['geo_enabled'],
+				content['statuses'][x]['user']['screen_name'].encode("ascii" , "ignore"),
+				content['statuses'][x]['user']['name'].encode("ascii" , "ignore"),
+				]
+				# print s
+				# for i in range(0 , len(s)):
+					# s[i] = unicode(s[i]).encode("utf-8")
+			
+				# print s
+				f.writerow(s)
+				# f.writerow(s)
+				# unicode(content['statuses'][x]['user']['screen_name']).replace("\/","/"),
+				# unicode(content['statuses'][x]['user']['name'] , errors='ignore'),
+				# str(content['statuses'][x]['geo']['coordinates'][0]) + ',' + str(content['statuses'][x]['geo']['coordinates'][1]),
+				# content['statuses'][x]['place']['full_name'],
+				# content['statuses'][x]['place']['country'].encode("utf-8")
+		
+			last_index = len(content['statuses']) -1
+			min_id = content['statuses'][last_index]['id']
+			max_id = content['statuses'][0]['id']
+			min_id = min_id -1
 
-		i+=1
-fp.close()
+			i+=1
+	fp.close()
+	
+fetch_tweets_till_date()
 
 #writing to json file	
 # closing the file after writing the data to it
