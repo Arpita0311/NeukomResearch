bb0983c335ad79b12c1c3668a274ad3e56ca5620
Arpita Jauhari <arpitajauhari@Arpitas-MacBook-Pro.local>
2015-07-12 20:02:24 -0400

    temp

--- test.py
+++ test.py
@@ -13,10 +13,10 @@ def fetch_tweets_till_date() :
 					"tweet_language","tweet_retweet_count","tweet_retweeted","tweet_source","tweet_text",
 					"user_contributors_enabled", "user_created_at","user_description", "user_Id","user_friend_count", 
 					"user_time_zone", "user_location", "user_geo_enabled" , "user_screen_name", "user_name","user_follower_count",
-					"user_friend_count", "user_favorities_count", "user_status_count", "user_language", 
-					"user_profile_background_image_url", "user_profile_image_url", "place_coordinate_lat_A", "place_coordinate_lng_A",
+					"user_favorities_count", "user_status_count", "user_language", 
+					"user_profile_background_image_url", "user_profile_image_url","place_coordinate_lat_A", "place_coordinate_lng_A",
 					"place_coordinate_lat_B","place_coordinate_lng_B","place_coordinate_lat_C","place_coordinate_lng_C","place_coordinate_lat_D",
-					"place_coordinate_lng_D","place_country",
+					"place_coordinate_lng_D", "place_country",
 					"place_full_name", "place_type"])
 					
 
@@ -58,7 +58,9 @@ def fetch_tweets_till_date() :
 			# 	print "Tweet id is " + str(tweet['id']) + " by " + str(tweet['user']['screen_name'])
 
 			for x in range(0 , len(content['statuses'])):
-
+				
+				
+				
 				lat_lng1 = content['statuses'][x].get('coordinates',None)
 				if lat_lng1 :
 					lat_lng1 = lat_lng1.get('coordinates')
@@ -70,20 +72,47 @@ def fetch_tweets_till_date() :
 					lat1_str = ''
 					lng1_str = ''
 					
+				place = content['statuses'][x].get('place',None)
 					
-				lat_lng2 = content['statuses'][x]['place']['bounding_box']['coordinates']
-				if lat_lng2 :
-					# lat_lng2 = lat_lng2.get('coordinates')
-					latA_str = str(lat_lng2[0][0])
-					lngA_str = str(lat_lng2[0][1])
-					latB_str = str(lat_lng2[1][0])
-					lngB_str = str(lat_lng2[1][1])
-					latC_str = str(lat_lng2[2][0])
-					lngC_str = str(lat_lng2[2][1])
-					latD_str = str(lat_lng2[3][0])
-					lngD_str = str(lat_lng2[3][1])
+				if place :
+					lat_lng3 = place.get('bounding_box',None)
+					if lat_lng3:
+						lat_lng2 = lat_lng3.get('coordinates', None)
+						if lat_lng2:
+							latA_str = str(lat_lng2[0][0][0])
+							lngA_str = str(lat_lng2[0][0][1])
+							latB_str = str(lat_lng2[0][1][0])
+  							lngB_str = str(lat_lng2[0][1][1])
+  							latC_str = str(lat_lng2[0][2][0])
+ 							lngC_str = str(lat_lng2[0][2][1])
+  							latD_str = str(lat_lng2[0][3][0])
+  							lngD_str = str(lat_lng2[0][3][1])
+						else:
+							latA_str = ''
+							lngA_str = ''
+							latB_str = ''
+							lngB_str = ''
+							latC_str = ''
+							lngC_str = ''
+							latD_str = ''
+							lngD_str = ''
+					else:
+						latA_str = ''
+						lngA_str = ''
+						latB_str = ''
+						lngB_str = ''
+						latC_str = ''
+						lngC_str = ''
+						latD_str = ''
+						lngD_str = ''
+					full_name= place['full_name']
+					country = place['country']
+					place_type=place['place_type']
 					
 				else:
+					full_name = ''
+					country=''
+					place_type=''
 					latA_str = ''
 					lngA_str = ''
 					latB_str = ''
@@ -92,13 +121,22 @@ def fetch_tweets_till_date() :
 					lngC_str = ''
 					latD_str = ''
 					lngD_str = ''
-					
 
 				s = [
 				content['statuses'][x]['id'],
-				content['statuses'][x]['user']['id'] ,
-				lat_str,
-				lng_str,
+				content['statuses'][x]['created_at'],
+				content['statuses'][x]['favorite_count'],
+				lat1_str,
+				lng1_str,
+				content['statuses'][x]['lang'],
+				content['statuses'][x]['retweet_count'],
+				content['statuses'][x]['retweeted'],
+				content['statuses'][x]['source'].encode("ascii" , "ignore"),
+				content['statuses'][x]['text'].encode("ascii" , "ignore"),
+				content['statuses'][x]['user']['contributors_enabled'],
+				content['statuses'][x]['user']['created_at'],
+				content['statuses'][x]['user']['description'].encode("ascii" , "ignore"),
+				content['statuses'][x]['user']['id'],
 				content['statuses'][x]['user']['friends_count'],
 				content['statuses'][x]['user']['time_zone'],
 				content['statuses'][x]['user']['location'].encode("ascii" , "ignore"),
@@ -106,28 +144,22 @@ def fetch_tweets_till_date() :
 				content['statuses'][x]['user']['screen_name'].encode("ascii" , "ignore"),
 				content['statuses'][x]['user']['name'].encode("ascii" , "ignore"),
 				content['statuses'][x]['user']['followers_count'],
-				content['statuses'][x]['user']['friends_count'],
 				content['statuses'][x]['user']['favourites_count'],
 				content['statuses'][x]['user']['statuses_count'],
 				content['statuses'][x]['user']['lang'],
 				content['statuses'][x]['user']['profile_background_image_url'].encode("ascii" , "ignore"),
 				content['statuses'][x]['user']['profile_image_url'].encode("ascii" , "ignore"),
 				latA_str,
-				lngA_str,
-				latB_str,
+ 				lngA_str,
+ 				latB_str,
 				lngB_str,
-				latC_str,
-				lngC_str,
-				latD_str,
-				lngD_str,
-				content['statuses'][x]['place']['country'],
-				content['statuses'][x]['place']['full_name'],
-				content['statuses'][x]['place']['place_type'],
-				content['statuses'][x]['user']['geo_enabled'],
-				content['statuses'][x]['user']['profile_background_image_url'].encode("ascii" , "ignore"),
-				content['statuses'][x]['user']['profile_image_url'].encode("ascii" , "ignore"),
-				
-
+  				latC_str,
+  				lngC_str,
+  				latD_str,
+  				lngD_str,
+				country,
+				full_name,
+				place_type
 				]
 				# print s
 				# for i in range(0 , len(s)):
@@ -165,6 +197,9 @@ f.writerow (["tweet ID","user ID","tweet coordinates",
 			"friend_Count","user location","geo_enabled",
 			"Screen name","user name","user coordinates
 			"place tagged","place_Country","place coordinates"])
+			"place_coordinate_lat_A", "place_coordinate_lng_A",
+								"place_coordinate_lat_B","place_coordinate_lng_B","place_coordinate_lat_C","place_coordinate_lng_C","place_coordinate_lat_D",
+								"place_coordinate_lng_D",
 
 
 '''	
